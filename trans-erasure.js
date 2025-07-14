88b60dc5141af5d7430c03ef4e717de96453c876
jkavanagh00 <jkavanagh00@gmail.com>
2025-07-14 15:08:44 +0200

    Create trans-erasure.js

---
+++ trans-erasure.js
@@ -0,0 +1,5 @@
+const chromosomeCheck = sperm => sperm === 'XY' && transErasure ? "Congratulations! You're going to have a son." : "Congratulations! You're going to have a daughter.";
+
+const transErasure = () => true;
+
+// a response to https://www.codewars.com/kata/56530b444e831334c0000020 //
